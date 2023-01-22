845b101ff5d86ab8a029ad2ee424ceb33127810c
Oleksii Kryvtsov <nightmarelie@gmail.com>
2023-01-21 22:17:36 -0800

    feat(server): add log property for fastify adapter

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -15,7 +15,7 @@ import { AppModule } from './app/app.module';
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
-    new FastifyAdapter()
+    new FastifyAdapter({ logger: true })
   );
 
   const globalPrefix = 'api';
