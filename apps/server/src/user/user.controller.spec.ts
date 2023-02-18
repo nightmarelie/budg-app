8076bdaef34e48b186b98c5b4fa2364880661f28
Oleksii Kryvtsov <nightmarelie@gmail.com>
2023-02-17 21:39:56 -0800

    feat(server): fix tests

--- apps/server/src/user/user.controller.spec.ts
+++ apps/server/src/user/user.controller.spec.ts
@@ -1,5 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
 import { UserController } from './user.controller';
+import { User } from './user.entity';
+import { UserService } from './user.service';
 
 describe('UserController', () => {
   let controller: UserController;
@@ -7,6 +10,13 @@ describe('UserController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
+      providers: [
+        UserService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: {}, // FIXME: Mock the repository
+        },
+      ],
     }).compile();
 
     controller = module.get<UserController>(UserController);
