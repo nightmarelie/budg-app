c56f021494cc07970d573baab9b0a7a4085965f5
Oleksii Kryvtsov <nightmarelie@gmail.com>
2023-02-17 18:13:08 -0800

    feat(server): create user in transaction way

--- apps/server/src/user/user.service.ts
+++ apps/server/src/user/user.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
 import { User } from './user.entity';
 
 @Injectable()
@@ -8,6 +8,7 @@ export class UserService {
   constructor(
     @InjectRepository(User)
     private usersRepository: Repository<User>,
+    private dataSource: DataSource,
   ) {}
 
   findAll(): Promise<User[]> {
@@ -20,5 +21,24 @@ export class UserService {
 
   async remove(id: string): Promise<void> {
     await this.usersRepository.delete(id);
+  }
+
+  async createMany(users: User[]) {
+    const queryRunner = this.dataSource.createQueryRunner();
+
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+    try {
+      for (const user in users) {
+        await queryRunner.manager.save(user);
+      }
+      await queryRunner.commitTransaction();
+    } catch (err) {
+      // since we have errors lets rollback the changes we made
+      await queryRunner.rollbackTransaction();
+    } finally {
+      // you need to release a queryRunner which was manually instantiated
+      await queryRunner.release();
+    }
   }
 }
