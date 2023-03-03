784c6aad88d86a463e86398af038f442b9a254d0
Oleksii Kryvtsov <nightmarelie@gmail.com>
2023-03-02 22:01:30 -0800

    feat(server): add health check for db

--- apps/server/src/health/health.controller.ts
+++ apps/server/src/health/health.controller.ts
@@ -4,6 +4,7 @@ import {
   HealthCheckService,
   HttpHealthIndicator,
   HealthCheck,
+  TypeOrmHealthIndicator,
 } from '@nestjs/terminus';
 
 const COLLECTION_NAME = 'health';
@@ -14,6 +15,7 @@ export class HealthController {
   constructor(
     private readonly health: HealthCheckService,
     private readonly http: HttpHealthIndicator,
+    private readonly db: TypeOrmHealthIndicator,
   ) {}
 
   @Get()
@@ -21,6 +23,7 @@ export class HealthController {
   check() {
     return this.health.check([
       () => this.http.pingCheck('api', 'http://localhost:3100/api'), // FIXME: put it to config
+      () => this.db.pingCheck('database'), // FIXME: put it to config
     ]);
   }
 }
