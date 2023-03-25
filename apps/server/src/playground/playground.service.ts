257c089b67cb6b0bf504f2da5798ba9a09ac589b
Oleksii Kryvtsov <nightmarelie@gmail.com>
2023-03-25 14:06:04 -0700

    feat(server): add use of cache manager

--- apps/server/src/playground/playground.service.ts
+++ apps/server/src/playground/playground.service.ts
@@ -1,4 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { CACHE_MANAGER, Inject, Injectable } from '@nestjs/common';
+import { Cache } from 'cache-manager';
 
 @Injectable()
-export class PlaygroundService {}
+export class PlaygroundService {
+  constructor(@Inject(CACHE_MANAGER) private cacheManager: Cache) {}
+
+  async getCachedValue(key: string): Promise<string> {
+    return this.cacheManager.get(key);
+  }
+
+  async setCachedValue(key: string, value: string): Promise<void> {
+    await this.cacheManager.set(key, value);
+  }
+}
