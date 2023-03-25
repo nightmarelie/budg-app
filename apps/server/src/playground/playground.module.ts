000c9aff0ee45cae1f4d0e2b2ad50fb9d4c5da4c
Oleksii Kryvtsov <nightmarelie@gmail.com>
2023-03-24 22:16:08 -0700

    feat(server): add playground

--- apps/server/src/playground/playground.module.ts
+++ apps/server/src/playground/playground.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { OrderCreatedListener } from './listeners/order.listener';
 import { PlaygroundController } from './playground.controller';
+import { PlaygroundService } from './playground.service';
 
 @Module({
-  providers: [OrderCreatedListener],
+  providers: [OrderCreatedListener, PlaygroundService],
   controllers: [PlaygroundController],
 })
 export class PlaygroundModule {}
