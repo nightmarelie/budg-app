e8bffe1bb70b855da2a5c0626ef9a080ba9fcc6e
Oleksii Kryvtsov <nightmarelie@gmail.com>
2023-03-29 17:49:28 -0700

    feat(server): register queue

--- apps/server/src/playground/playground.module.ts
+++ apps/server/src/playground/playground.module.ts
@@ -1,9 +1,15 @@
+import { BullModule } from '@nestjs/bull';
 import { Module } from '@nestjs/common';
 import { OrderCreatedListener } from './listeners/order.listener';
 import { PlaygroundController } from './playground.controller';
 import { PlaygroundService } from './playground.service';
 
 @Module({
+  imports: [
+    BullModule.registerQueue({
+      name: 'playground',
+    }),
+  ],
   providers: [OrderCreatedListener, PlaygroundService],
   controllers: [PlaygroundController],
 })
